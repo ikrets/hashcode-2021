479b911258d04da31431a4b85b9584009a8373b4
Ilia <ilia.krets@faro.com>
2021-02-21 16:37:05 +0100

    Write random solution to file

--- try_random.py
+++ try_random.py
@@ -6,6 +6,7 @@ import tqdm
 
 input_file = sys.argv[1]
 n_iterations = int(sys.argv[2])
+output_file = sys.argv[3]
 
 with open(sys.argv[1], "r") as fp:
     lines = fp.readlines()
@@ -30,7 +31,7 @@ def produce_random():
     random.shuffle(indices)
     current_index = 0
 
-    while len(available_pizzas) > min(whats_left()):
+    while whats_left() and len(available_pizzas) > min(whats_left()):
         random_team_size = random.choice(whats_left())
         random_indices = indices[current_index: current_index + random_team_size]
         current_index += random_team_size
@@ -51,6 +52,12 @@ def score(solution):
             all_ingridients.update(pizzas[pizza][1])
         total_score += len(all_ingridients) ** 2
     return total_score
+
+random_solution = produce_random()
+with open(output_file, "w") as fp:
+    fp.write(f"{len(random_solution)}\n")
+    for s in random_solution:
+        fp.write(f"{' '.join(str(ss) for ss in s)}\n")
 
 random_scores = []
 for i in tqdm.trange(n_iterations):
