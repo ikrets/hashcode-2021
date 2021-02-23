6971d0cc1669e3627c3a9c4ca3ce77be62c4c90a
PavelYasukevich <ptntbox@gmail.com>
2021-02-23 23:35:06 +0500

    data and c sol

---
+++ solution_c.py
@@ -0,0 +1,80 @@
+import sys
+
+input_file, output_file = sys.argv[1:3]
+
+
+class Ride:
+    def __init__(self, idx, a, b, x, y, min_start, max_finish):
+        self.idx = idx
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        self.min_start = min_start
+        self.max_finish = max_finish
+
+    def __str__(self):
+        return f'{self.idx} {self.a} {self.b} {self.x} {self.y} {self.min_start} {self.max_finish}'
+
+    @property
+    def distance(self):
+        return abs(self.a-self.x) + abs(self.b-self.y)
+
+
+class Vehicle:
+    def __init__(self, idx, row=0, col=0, steps_left=0, rides_done=None):
+        self.idx = idx
+        self.row = row
+        self.col = col
+        self.steps_left = steps_left
+        self.rides_done = rides_done if rides_done is not None else []
+
+
+    def __str__(self):
+        return f'{self.idx} {self.row} {self.col} {self.steps_left} {self.rides_done}'
+
+
+def distance(a, b, x, y):
+    return abs(a-x) + abs(b-y)
+
+ride_list = []
+
+with open(input_file, "r") as f:
+    first_line = f.readline()
+    ROWS, COLS, VEHICLES, RIDES, BONUS, STEPS = map(int, first_line.split())
+    print(ROWS, COLS, VEHICLES, RIDES, BONUS, STEPS)
+    lines = f.readlines()
+    for idx, line in enumerate(lines):
+        ride = Ride(idx, *map(int, line.split()))
+        ride_list.append(ride)
+
+cars = [Vehicle(idx) for idx in range(VEHICLES)]
+
+
+while STEPS > 0 and len(ride_list) > 0:
+    for car in cars:
+        if car.steps_left == 0:
+            ride_list.sort(
+                key=lambda ride: (
+                    -distance(car.row, car.col, ride.a, ride.b), # to pop closest to car
+                    ride.distance, # longest distance first
+                )
+            )
+            ride = ride_list.pop()
+            car.steps_left = ride.distance
+            car.rides_done.append(ride.idx)
+
+        car.steps_left -= 1
+        STEPS -= 1
+
+
+
+with open(output_file, "w") as f:
+    for car in cars:
+        M = len(car.rides_done)
+        rides_idxes = car.rides_done
+        f.write(f'{M} {rides_idxes}\n')
+
+
+
+
