24c0373cdc65f90a8e79e62859e8c51401e36084
User <user@mail.com>
2021-03-10 22:19:21 +0100

    simple validate submittion and score calculate

---
+++ validate.py
@@ -0,0 +1,185 @@
+import os, sys
+from collections import defaultdict, namedtuple, Counter, deque
+from dataclasses import dataclass, field
+from itertools import chain
+from pprint import pprint
+import pandas as pd
+from tqdm import tqdm
+import random
+from typing import List, Dict, Tuple, Deque
+import statistics
+
+# ● The rst line contains ve numbers:
+# ○ an integer D (1 ≤ D ≤ 10 4)  - the duration of the simulation, in seconds,
+# ○ an integer I (2 ≤ I ≤ 10 5) - the number of intersections (with IDs from 0 to I -1 ),
+# ○ an integer S (2 ≤ S ≤ 10 5) - the number of streets,
+# ○ an integer V (1 ≤ V ≤ 10 3) - the number of cars,
+# ○ an integer F (1 ≤ F ≤ 10 3) - the bonus points for each car that reaches # its destination before time D
+#
+#   ● The next S lines contain descriptions of streets. Each line contains:
+#   ○ two integers B and E (0 ≤ B < I , 0 ≤ E < I ) - the intersections at the start and the end of the street, respectively,
+#   ○ the street name (a string consisting of between 3 and 30 lowercase ASCII characters a -z and the character - ),
+#   ○ an integer L (1 ≤ L ≤ D ) - the time it takes a car to get from the beginning to the end of that street.
+#
+#   ● The next V lines describe the paths of each car. Each line contains:
+#   ○ an integer P (2 ≤ P ≤ 10 3) - the number of streets that the car wants to travel,
+#   ○ followed by P names of the streets: The car starts at the end of the first street
+#     (i.e. it waits for the green light to move to the next street)
+#     and follows the path until the end of the last street. The path of a car is
+#     always valid, i.e. the streets will be connected by intersections.
+
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+print(f'*** Input [{in_file}], Solution: [{out_file}] ***')
+TRACE = int(sys.argv[3]) if len(sys.argv) > 3 else 0
+
+@dataclass
+class Street:
+    id_: int
+    B: int
+    E: int
+    name: str
+    L: int
+
+Streets: Dict[str, Street] = {}
+
+@dataclass
+class Car:
+    path: List[str]
+    # pos: Tuple[int, int] = field(init=False)
+
+Cars: List[Car] = []
+
+stat_path_length = Counter()
+stat_street_end = Counter()
+stat_car_end_street = Counter()
+stat_cars_through_intersectoin = Counter()
+
+upper_bound_score = 0
+with open(in_file) as fin:
+    D, I, S, V, F = map(int, fin.readline().split())
+
+    for _ in range(S):
+        B, E, name, L = fin.readline().split()
+        Streets[name] = Street(len(Streets), int(B), int(E), name, int(L))
+        stat_street_end[E] += 1
+
+    for v in range(V):
+        P, *streets = fin.readline().split()
+        assert int(P) > 1 and int(P) == len(set(streets)) == len(streets), f'{P} {streets}'
+        Cars.append(Car(streets))
+        assert all(_ in Streets for _ in Cars[-1].path)
+
+        tt = sum(Streets[s].L for s in streets[1:])
+        upper_bound_score += F + (D - tt) if tt <= D else 0
+
+        stat_path_length[streets[1]] += 1
+        stat_car_end_street[Streets[streets[-1]].B] += 1
+
+        for s in streets:
+            stat_cars_through_intersectoin[Streets[s].E] += 1
+
+assert S == len(Streets)
+assert V == len(Cars)
+
+print(f'\nSimulation Time: {D}, Intersections: {I}, Streets: {S}, Cars: {V}, Bonus: {F}')
+print(f'\nUpper bound total score: {upper_bound_score}')
+
+print(f'\nsecond_street: {len(stat_path_length)} {stat_path_length.most_common(10)}')
+print(f'\nstat_street_end: {len(stat_street_end)} {stat_street_end.most_common(10)}')
+print(f'\nstat_car_end_street: {len(stat_car_end_street)} {stat_car_end_street.most_common(10)}')
+print(f'\n stat_cars_through_intersectoin: {len(stat_cars_through_intersectoin)} {stat_cars_through_intersectoin.most_common(10)}')
+
+total_score = 0
+
+@dataclass
+class Intersection:
+    schedule: Dict[str, int]
+
+    cycle_time: int = 0
+    def __post_init__(self):
+        self.cycle_time = sum(t for t in self.schedule.values())
+
+    queues: Dict[str, Deque[Tuple[int, int]]] = field(default_factory=lambda: defaultdict(deque))
+
+    def greenLight(self, t):
+        assert len(self.schedule) >= 1
+        if len(self.schedule) == 1:
+            return next(iter(self.schedule.keys()))
+
+        t = t if t <= self.cycle_time else t % self.cycle_time
+        for k, v in self.schedule.items():
+            if t <= v:
+                return k
+            t -= v
+
+        assert False
+
+
+Is = [ None for _ in range(I) ] # list of intersections by id == index
+
+with open(out_file) as fo:
+    for _ in range(int(fo.readline())): # number of schedules (== intersections with assigned schedule)
+        inter_id = int(fo.readline())
+        assert 0 <= inter_id <= I, f'0 <= {inter_id} <= {I}: invalid intersection'
+        assert Is[inter_id] == None, f'Schedule for intersection [{inter_id}] is duplicated!'
+
+        num_traffic_light_slots = int(fo.readline())
+        assert num_traffic_light_slots > 0
+
+        schedule = {}
+        for _ in range(num_traffic_light_slots):
+            street_name, time_t = fo.readline().split()
+            assert street_name in Streets, f'"{street_name}" not among {len(Streets)} known Streets'
+            assert street_name not in schedule, f'{street_name} not in {sol.I[inter_id].keys()}: duplicated street schedule'
+            assert int(time_t) > 0, f't {time_t} > 0 for {street_name} in intersection {inter_id}'
+            schedule[street_name] = int(time_t)
+
+        Is[inter_id] = Intersection(schedule)
+
+assert sum(1 for i in Is if i) > 0, 'no schedules in solution'
+
+assert type(Cars) == list
+for i, car in enumerate(Cars):
+    start_street = car.path.pop(0)
+    inter_id = Streets[start_street].E
+    if Is[inter_id]:
+        Is[inter_id].queues[start_street].append((i, 0))
+    else:
+        Cars[i] = None
+
+assert sum(1 for c in Cars if c) == sum(sum(len(l) for l in i.queues.values()) for i in Is if i) , f'{V} == {_s}'
+
+total_score = 0
+for t in tqdm(range(D)):
+    for i, inter in enumerate(Is):
+        if not inter:
+            continue
+
+        street = inter.greenLight(t)
+        if TRACE: print(f'T[{t}] | inter: {i} - Green: {street}')
+        if street not in inter.queues or not inter.queues[street]:
+            continue
+
+        car_id, arrival_time = inter.queues[street][0]
+        if TRACE: print(f'\tcar: {car_id} time: {arrival_time}')
+        if arrival_time > t:
+            continue
+
+        inter.queues[street].popleft()
+        car = Cars[car_id]
+        next_street_name = car.path.pop(0)
+        next_street = Streets[next_street_name]
+        if TRACE: print(f'\t\tnext_street -->> {next_street_name}')
+
+        if not car.path:
+            T = t + next_street.L
+            if T < D:
+                total_score += F + (D - T - 1)
+        else:
+            next_inter_id = next_street.E
+            Is[next_inter_id].queues[next_street_name].append((car_id, t + next_street.L))
+
+    if TRACE: print('------------------------------------------------------')
+
+print(f'Total computed score: {total_score}')
